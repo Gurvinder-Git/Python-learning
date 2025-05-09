617a14b70bbd05576850c0a1b180eb7ce7f2f536
Gurvinder-Git <guvinder.imp13@gmail.com>
2025-05-09 18:44:53 +0530

    Add python file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Python-learning
-This is my first Git Repository
+This is my first Git Repository.
 <br/>
 Author - Gurvinder Singh
---
+++ first.py
@@ -0,0 +1,5 @@
+print("Comparison between two Numbers")
+if 5 < 9:
+    print("9 is greater than 5")
+else:
+    print("5 is greater than 9")
